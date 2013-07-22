aa38a4516f1162f906f23e7f4f7ce7b7ca3b4dde
Povilas Kanapickas <tir5c3@yahoo.co.uk>
2013-07-23 00:19:18 +0300

    Update the copyright to list my real name

--- simdpp/simd/extract.h
+++ simdpp/simd/extract.h
@@ -1,5 +1,5 @@
 /*  libsimdpp
-    Copyright (C) 2011-2013  p12 tir5c3@yahoo.co.uk
+    Copyright (C) 2011-2013  Povilas Kanapickas tir5c3@yahoo.co.uk
 
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
