1364fb054490f9498354ec5b463716f9acef2058
Povilas Kanapickas <tir5c3@yahoo.co.uk>
2013-10-19 06:30:59 +0300

    shuffle: Improve the AVX2 implementation of int64x4 shuffling

--- simdpp/simd/shuffle_generic.h
+++ simdpp/simd/shuffle_generic.h
@@ -418,7 +418,11 @@ template<unsigned s0, unsigned s1>
 basic_int64x4 permute(basic_int64x4 a)
 {
     static_assert(s0 < 2 && s1 < 2, "Selector out of range");
+#if SIMDPP_USE_AVX2
+    return _mm256_permute4x64_epi64(a, s0 | s1<<2 | s0<<4 | s1<<6);
+#else
     return permute<s0,s1,s0+2,s1+2>(a);
+#endif
 }
 /// @}
 
@@ -453,7 +457,9 @@ template<unsigned s0, unsigned s1>
 float64x4 permute(float64x4 a)
 {
     static_assert(s0 < 2 && s1 < 2, "Selector out of range");
-#if SIMDPP_USE_AVX
+#if SIMDPP_USE_AVX2
+    return _mm256_permute4x64_pd(a, s0 | s1<<2 | s0<<4 | s1<<6);
+#elif SIMDPP_USE_AVX
     return _mm256_permute_pd(a, s0 | s1<<1 | s0<<2 | s1<<3);
 #else
     return permute<s0,s1,s0+2,s1+2>(a);
