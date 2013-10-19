823804d0942cb0948eb1fce6df9178ad11ad20ac
Povilas Kanapickas <tir5c3@yahoo.co.uk>
2013-10-19 06:31:05 +0300

    test: Update floating-point math tests to cover 32-byte vectors too

--- test/tests/math_fp.cc
+++ test/tests/math_fp.cc
@@ -44,82 +44,82 @@ void test_math_fp(TestResults& res)
     float inff = std::numeric_limits<float>::infinity();
     double inf = std::numeric_limits<double>::infinity();
 
-    //float32x4
+    // Vectors with 32-bit floating-point elements
     {
-        float32x4 s[] = {
-            float32x4::make_const(0.0f, -0.0f, 0.0f, -0.0f),
-            float32x4::make_const(1.0f, 2.0f, 3.0f, 4.0f),
-            float32x4::make_const(-1.0f, -2.0f, -3.0f, -4.0f),
-            float32x4::make_const(67500000.0f, 67500001.0f, 67500002.0f, 67500003.0f),
-            float32x4::make_const(-67500000.0f, -67500001.0f, -67500002.0f, -67500003.0f),
-            float32x4::make_const(nanf, nanf, nanf, nanf),
-            float32x4::make_const(inff, inff, inff, inff),
-            float32x4::make_const(-inff, -inff, -inff, -inff),
+        float32x8 s[] = {
+            float32x8::make_const(0.0f, -0.0f, 0.0f, -0.0f),
+            float32x8::make_const(1.0f, 2.0f, 3.0f, 4.0f),
+            float32x8::make_const(-1.0f, -2.0f, -3.0f, -4.0f),
+            float32x8::make_const(67500000.0f, 67500001.0f, 67500002.0f, 67500003.0f),
+            float32x8::make_const(-67500000.0f, -67500001.0f, -67500002.0f, -67500003.0f),
+            float32x8::make_const(nanf, nanf, nanf, nanf),
+            float32x8::make_const(inff, inff, inff, inff),
+            float32x8::make_const(-inff, -inff, -inff, -inff),
         };
 
-        TEST_ALL_COMB_HELPER(tc, float32x4, add, s, 4);
-        TEST_ALL_COMB_HELPER(tc, float32x4, sub, s, 4);
-        TEST_ALL_COMB_HELPER(tc, float32x4, mul, s, 4);
+        TEST_ALL_COMB_HELPER16X2_2(tc, float32x8, add, s, 4);
+        TEST_ALL_COMB_HELPER16X2_2(tc, float32x8, sub, s, 4);
+        TEST_ALL_COMB_HELPER16X2_2(tc, float32x8, mul, s, 4);
         tc.set_precision(1);
-        TEST_ALL_COMB_HELPER(tc, float32x4, div, s, 4);
+        TEST_ALL_COMB_HELPER16X2_2(tc, float32x8, div, s, 4);
         tc.unset_precision();
 
-        TEST_ARRAY_HELPER1(tc, float32x4, abs, s);
-        TEST_ARRAY_HELPER1(tc, float32x4, sign, s);
-        TEST_ARRAY_HELPER1(tc, float32x4, neg, s);
+        TEST_ARRAY_HELPER16X2_1(tc, float32x8, abs, s);
+        TEST_ARRAY_HELPER16X2_1(tc, float32x8, sign, s);
+        TEST_ARRAY_HELPER16X2_1(tc, float32x8, neg, s);
 
-        float32x4 snan[] = {
-            float32x4::make_const(1.0f, 2.0f, 3.0f, 4.0f),
-            float32x4::make_const(-1.0f, -2.0f, -3.0f, -4.0f),
-            float32x4::make_const(67500000.0f, 67500001.0f, 67500002.0f, 67500003.0f),
-            float32x4::make_const(-67500000.0f, -67500001.0f, -67500002.0f, -67500003.0f),
-            float32x4::make_const(inff, inff, inff, inff),
-            float32x4::make_const(-inff, -inff, -inff, -inff),
+        float32x8 snan[] = {
+            float32x8::make_const(1.0f, 2.0f, 3.0f, 4.0f),
+            float32x8::make_const(-1.0f, -2.0f, -3.0f, -4.0f),
+            float32x8::make_const(67500000.0f, 67500001.0f, 67500002.0f, 67500003.0f),
+            float32x8::make_const(-67500000.0f, -67500001.0f, -67500002.0f, -67500003.0f),
+            float32x8::make_const(inff, inff, inff, inff),
+            float32x8::make_const(-inff, -inff, -inff, -inff),
         };
-        TEST_ALL_COMB_HELPER(tc, float32x4, min, snan, 4);
-        TEST_ALL_COMB_HELPER(tc, float32x4, max, snan, 4);
+        TEST_ALL_COMB_HELPER16X2_2(tc, float32x8, min, snan, 4);
+        TEST_ALL_COMB_HELPER16X2_2(tc, float32x8, max, snan, 4);
     }
 
-    //float64x2
+    // Vectors with 64-bit floating-point elements
     {
-        float64x2 s[] = {
-            float64x2::make_const(0.0, -0.0),
-            float64x2::make_const(1.0, 2.0),
-            float64x2::make_const(3.0, 4.0),
-            float64x2::make_const(-1.0, -2.0),
-            float64x2::make_const(-3.0, -4.0),
-            float64x2::make_const(63100000000000000.0, 63100000000000004.0),
-            float64x2::make_const(63100000000000008.0, 63100000000000012.0),
-            float64x2::make_const(-63100000000000000.0, -63100000000000004.0),
-            float64x2::make_const(-63100000000000008.0, -63100000000000012.0),
-            float64x2::make_const(nan, nan),
-            float64x2::make_const(inf, inf),
-            float64x2::make_const(-inf, -inf),
+        float64x4 s[] = {
+            float64x4::make_const(0.0, -0.0),
+            float64x4::make_const(1.0, 2.0),
+            float64x4::make_const(3.0, 4.0),
+            float64x4::make_const(-1.0, -2.0),
+            float64x4::make_const(-3.0, -4.0),
+            float64x4::make_const(63100000000000000.0, 63100000000000004.0),
+            float64x4::make_const(63100000000000008.0, 63100000000000012.0),
+            float64x4::make_const(-63100000000000000.0, -63100000000000004.0),
+            float64x4::make_const(-63100000000000008.0, -63100000000000012.0),
+            float64x4::make_const(nan, nan),
+            float64x4::make_const(inf, inf),
+            float64x4::make_const(-inf, -inf),
         };
 
-        TEST_ALL_COMB_HELPER(tc, float64x2, add, s, 4);
-        TEST_ALL_COMB_HELPER(tc, float64x2, sub, s, 4);
-        TEST_ALL_COMB_HELPER(tc, float64x2, mul, s, 4);
-        TEST_ALL_COMB_HELPER(tc, float64x2, div, s, 4);
+        TEST_ALL_COMB_HELPER16X2_2(tc, float64x4, add, s, 4);
+        TEST_ALL_COMB_HELPER16X2_2(tc, float64x4, sub, s, 4);
+        TEST_ALL_COMB_HELPER16X2_2(tc, float64x4, mul, s, 4);
+        TEST_ALL_COMB_HELPER16X2_2(tc, float64x4, div, s, 4);
 
-        TEST_ARRAY_HELPER1(tc, float64x2, abs, s);
-        TEST_ARRAY_HELPER1(tc, float64x2, sign, s);
-        TEST_ARRAY_HELPER1(tc, float64x2, neg, s);
+        TEST_ARRAY_HELPER16X2_1(tc, float64x4, abs, s);
+        TEST_ARRAY_HELPER16X2_1(tc, float64x4, sign, s);
+        TEST_ARRAY_HELPER16X2_1(tc, float64x4, neg, s);
 
-        float64x2 snan[] = {
-            float64x2::make_const(1.0, 2.0),
-            float64x2::make_const(3.0, 4.0),
-            float64x2::make_const(-1.0, -2.0),
-            float64x2::make_const(-3.0, -4.0),
-            float64x2::make_const(63100000000000000.0, 63100000000000004.0),
-            float64x2::make_const(63100000000000008.0, 63100000000000012.0),
-            float64x2::make_const(-63100000000000000.0, -63100000000000004.0),
-            float64x2::make_const(-63100000000000008.0, -63100000000000012.0),
-            float64x2::make_const(inf, inf),
-            float64x2::make_const(-inf, -inf),
+        float64x4 snan[] = {
+            float64x4::make_const(1.0, 2.0),
+            float64x4::make_const(3.0, 4.0),
+            float64x4::make_const(-1.0, -2.0),
+            float64x4::make_const(-3.0, -4.0),
+            float64x4::make_const(63100000000000000.0, 63100000000000004.0),
+            float64x4::make_const(63100000000000008.0, 63100000000000012.0),
+            float64x4::make_const(-63100000000000000.0, -63100000000000004.0),
+            float64x4::make_const(-63100000000000008.0, -63100000000000012.0),
+            float64x4::make_const(inf, inf),
+            float64x4::make_const(-inf, -inf),
         };
-        TEST_ALL_COMB_HELPER(tc, float64x2, min, snan, 4);
-        TEST_ALL_COMB_HELPER(tc, float64x2, max, snan, 4);
+        TEST_ALL_COMB_HELPER16X2_2(tc, float64x4, min, snan, 4);
+        TEST_ALL_COMB_HELPER16X2_2(tc, float64x4, max, snan, 4);
     }
 }
 
