9a5b9161279c1ec8b2056dc9b4c9abe0e2378c1b
Povilas Kanapickas <povilas@radix.lt>
2014-03-01 03:36:29 +0200

    traits: Fix vector and mask recognition
    The fixed implementation recognizes arbitrary length vectors, masks
    and vector expressions.

--- simdpp/types/traits.h
+++ simdpp/types/traits.h
@@ -33,7 +33,7 @@
 #endif
 
 #include <simdpp/types.h>
-
+#include <type_traits>
 namespace simdpp {
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 namespace SIMDPP_ARCH_NAMESPACE {
@@ -43,51 +43,37 @@ namespace SIMDPP_ARCH_NAMESPACE {
 template<class T>
 struct is_vector { static constexpr bool value = false; };
 
-template<> struct is_vector<float32x4>   { static constexpr bool value = true; };
-template<> struct is_vector<float64x2>   { static constexpr bool value = true; };
-template<> struct is_vector<float32x8>   { static constexpr bool value = true; };
-template<> struct is_vector<float64x4>   { static constexpr bool value = true; };
-template<> struct is_vector<int8x16>     { static constexpr bool value = true; };
-template<> struct is_vector<int16x8>     { static constexpr bool value = true; };
-template<> struct is_vector<int32x4>     { static constexpr bool value = true; };
-template<> struct is_vector<int64x2>     { static constexpr bool value = true; };
-template<> struct is_vector<int8x32>     { static constexpr bool value = true; };
-template<> struct is_vector<int16x16>    { static constexpr bool value = true; };
-template<> struct is_vector<int32x8>     { static constexpr bool value = true; };
-template<> struct is_vector<int64x4>     { static constexpr bool value = true; };
-template<> struct is_vector<uint8x16>    { static constexpr bool value = true; };
-template<> struct is_vector<uint16x8>    { static constexpr bool value = true; };
-template<> struct is_vector<uint32x4>    { static constexpr bool value = true; };
-template<> struct is_vector<uint64x2>    { static constexpr bool value = true; };
-template<> struct is_vector<uint8x32>    { static constexpr bool value = true; };
-template<> struct is_vector<uint16x16>   { static constexpr bool value = true; };
-template<> struct is_vector<uint32x8>    { static constexpr bool value = true; };
-template<> struct is_vector<uint64x4>    { static constexpr bool value = true; };
-template<> struct is_vector<gint8x16>   { static constexpr bool value = true; };
-template<> struct is_vector<gint16x8>   { static constexpr bool value = true; };
-template<> struct is_vector<gint32x4>   { static constexpr bool value = true; };
-template<> struct is_vector<gint64x2>   { static constexpr bool value = true; };
-template<> struct is_vector<gint8x32>   { static constexpr bool value = true; };
-template<> struct is_vector<gint16x16>  { static constexpr bool value = true; };
-template<> struct is_vector<gint32x8>   { static constexpr bool value = true; };
-template<> struct is_vector<gint64x4>   { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<float32<N,E>>   { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<float64<N,E>>   { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<int8<N,E>>      { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<int16<N,E>>     { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<int32<N,E>>     { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<int64<N,E>>     { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<uint8<N,E>>     { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<uint16<N,E>>    { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<uint32<N,E>>    { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<uint64<N,E>>    { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<gint8<N,E>>     { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<gint16<N,E>>    { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<gint32<N,E>>    { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<gint64<N,E>>    { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<mask_int8<N,E>> { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<mask_int16<N,E>>    { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<mask_int32<N,E>>    { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<mask_int64<N,E>>    { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<mask_float32<N,E>>  { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_vector<mask_float64<N,E>>  { static constexpr bool value = true; };
 
 /// Allows detection whether specific type is a simdpp mask
 template<class T>
 struct is_mask { static constexpr bool value = false; };
 
-template<> struct is_mask<mask_int8x16>  { static constexpr bool value = true; };
-template<> struct is_mask<mask_int8x32>  { static constexpr bool value = true; };
-template<> struct is_mask<mask_int16x8>  { static constexpr bool value = true; };
-template<> struct is_mask<mask_int16x16> { static constexpr bool value = true; };
-template<> struct is_mask<mask_int32x4>  { static constexpr bool value = true; };
-template<> struct is_mask<mask_int32x8>  { static constexpr bool value = true; };
-template<> struct is_mask<mask_int64x2>  { static constexpr bool value = true; };
-template<> struct is_mask<mask_int64x4>  { static constexpr bool value = true; };
-template<> struct is_mask<mask_float32x4> { static constexpr bool value = true; };
-template<> struct is_mask<mask_float32x8> { static constexpr bool value = true; };
-template<> struct is_mask<mask_float64x2> { static constexpr bool value = true; };
-template<> struct is_mask<mask_float64x4> { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_mask<mask_int8<N,E>>    { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_mask<mask_int16<N,E>>   { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_mask<mask_int32<N,E>>   { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_mask<mask_int64<N,E>>   { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_mask<mask_float32<N,E>> { static constexpr bool value = true; };
+template<unsigned N, class E> struct is_mask<mask_float64<N,E>> { static constexpr bool value = true; };
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 } // namespace SIMDPP_ARCH_NAMESPACE
