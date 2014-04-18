8df07f86a4c4d109ff7b9a3877c779a0eb546f14
Povilas Kanapickas <povilas@radix.lt>
2014-04-18 07:34:16 +0300

    permute: Fix circular header dependencies

--- simdpp/detail/insn/permute4.h
+++ simdpp/detail/insn/permute4.h
@@ -14,7 +14,6 @@
 
 #include <simdpp/types.h>
 #include <simdpp/core/make_shuffle_bytes_mask.h>
-#include <simdpp/detail/insn/shuffle_emul.h>
 #include <simdpp/detail/null/shuffle.h>
 #include <simdpp/sse/shuffle.h>
 #include <simdpp/neon/detail/shuffle_int16x8.h>
