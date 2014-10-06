f64dc68dd06794d7a0b5d88c86803945ce1a84b7
Povilas Kanapickas <povilas@radix.lt>
2014-10-06 07:03:34 +0300

    operators: Run tools/gen_operators.pl to generate the operators headers

---
+++ simdpp/operators/operators.h
@@ -0,0 +1,30 @@
+/*  Copyright (C) 2014  Povilas Kanapickas <povilas@radix.lt>
+
+    Distributed under the Boost Software License, Version 1.0.
+        (See accompanying file LICENSE_1_0.txt or copy at
+            http://www.boost.org/LICENSE_1_0.txt)
+*/
+
+#ifndef LIBSIMDPP_OPERATOR_OPERATOR_H
+#define LIBSIMDPP_OPERATOR_OPERATOR_H
+
+#include <simdpp/operator/bit_and.h>
+#include <simdpp/operator/bit_or.h>
+#include <simdpp/operator/bit_xor.h>
+#include <simdpp/operator/bit_not.h>
+#include <simdpp/operator/cmp_eq.h>
+#include <simdpp/operator/cmp_neq.h>
+#include <simdpp/operator/cmp_ge.h>
+#include <simdpp/operator/cmp_gt.h>
+#include <simdpp/operator/cmp_le.h>
+#include <simdpp/operator/cmp_lt.h>
+#include <simdpp/operator/f_add.h>
+#include <simdpp/operator/f_mul.h>
+#include <simdpp/operator/f_sub.h>
+#include <simdpp/operator/i_add.h>
+#include <simdpp/operator/i_mul.h>
+#include <simdpp/operator/i_shift_l.h>
+#include <simdpp/operator/i_shift_r.h>
+#include <simdpp/operator/i_sub.h>
+
+#endif
