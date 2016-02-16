fc2e1b35ff8972a8ebfd06c4fcd6776150b924f2
Povilas Kanapickas <povilas@radix.lt>
2016-02-16 16:20:01 +0200

    test: Add better support of running under simulator

--- test/main_test1.cc
+++ test/main_test1.cc
@@ -9,21 +9,56 @@
 #include "utils/test_results.h"
 #include <simdpp/setup_arch.h>
 #include <algorithm>
-#include <iostream>
 #include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <iomanip>
+#include <vector>
 
 #if __linux__
 #include <simdpp/dispatch/get_arch_linux_cpuinfo.h>
-
-inline simdpp::Arch get_arch() { return simdpp::get_arch_linux_cpuinfo(); }
-
-#elif SIMDPP_X86
-#include <simdpp/dispatch/get_arch_raw_cpuid.h>
-
-inline simdpp::Arch get_arch() { return simdpp::get_arch_raw_cpuid(); }
-#else
-#error "Can't test dispatcher on this platform"
 #endif
+#if SIMDPP_X86
+#include <simdpp/dispatch/get_arch_raw_cpuid.h>
+#endif
+
+inline simdpp::Arch get_arch(bool is_simulator)
+{
+    (void) is_simulator;
+    std::vector<simdpp::Arch> supported_archs;
+#if __linux__
+    if (!is_simulator)
+        supported_archs.push_back(simdpp::get_arch_linux_cpuinfo());
+#endif
+#if SIMDPP_X86
+    supported_archs.push_back(simdpp::get_arch_raw_cpuid());
+#endif
+    if (supported_archs.empty()) {
+        std::cerr << "No architecture information could be retrieved. Testing not supported\n";
+        std::exit(EXIT_FAILURE);
+    }
+
+    simdpp::Arch result = supported_archs.front();
+    for (unsigned i = 1; i < supported_archs.size(); ++i) {
+        if (supported_archs[i] != result) {
+            std::cerr << "Different CPU architecture evaluators return different results\n"
+                      << std::hex << (unsigned)result << " " << i << " "
+                      << std::hex << (unsigned)supported_archs[i] << "\n";
+            std::exit(EXIT_FAILURE);
+        }
+    }
+    return result;
+}
+
+void parse_args(int argc, char* argv[], bool& is_simulator)
+{
+    is_simulator = false;
+    for (int i = 1; i < argc; ++i)
+    {
+        if (std::strcmp(argv[i], "--simulator") == 0)
+            is_simulator = true;
+    }
+}
 
 /*  We test libsimdpp by comparing the results of the same computations done in
     different 'architectures'. That is, we build a list of results for each
@@ -31,10 +66,14 @@ inline simdpp::Arch get_arch() { return simdpp::get_arch_raw_cpuid(); }
     non-vectorized code). All tests are generated from the same source code,
     thus any differencies are likely to be caused by bugs in the library.
 */
-int main()
+int main(int argc, char* argv[])
 {
+    bool is_simulator = false;
+    parse_args(argc, argv, is_simulator);
+
+    simdpp::Arch current_arch = get_arch(is_simulator);
+
     std::ostream& err = std::cerr;
-    simdpp::Arch current_arch = get_arch();
     const auto& arch_list = ArchRegistration::arch_list();
     auto null_arch = std::find_if(arch_list.begin(), arch_list.end(),
                                   [](const ArchRegistration::Arch& a) -> bool
