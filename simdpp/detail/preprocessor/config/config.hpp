73736cf9a7508f22c85a1ed6a2aad1f376a81b2c
Povilas Kanapickas <povilas@radix.lt>
2017-10-25 23:08:39 +0300

    preprocessor: Update config

--- simdpp/detail/preprocessor/config/config.hpp
+++ simdpp/detail/preprocessor/config/config.hpp
@@ -27,27 +27,25 @@
 # define SIMDPP_PP_CONFIG_DMC() 0x0040
 #
 # ifndef SIMDPP_PP_CONFIG_FLAGS
-#    if defined(__GCCXML__)
-#        define SIMDPP_PP_CONFIG_FLAGS() (SIMDPP_PP_CONFIG_STRICT())
-#    elif defined(__WAVE__)
-#        define SIMDPP_PP_CONFIG_FLAGS() (SIMDPP_PP_CONFIG_STRICT())
-#    elif defined(__MWERKS__) && __MWERKS__ >= 0x3200
+#    if defined(__GCCXML__) || defined(__WAVE__) || defined(__MWERKS__) && __MWERKS__ >= 0x3200
 #        define SIMDPP_PP_CONFIG_FLAGS() (SIMDPP_PP_CONFIG_STRICT())
 #    elif defined(__EDG__) || defined(__EDG_VERSION__)
-#        if defined(_MSC_VER) && (defined(__INTELLISENSE__) || __EDG_VERSION__ >= 308)
+#        if defined(_MSC_VER) && !defined(__clang__) && (defined(__INTELLISENSE__) || __EDG_VERSION__ >= 308)
 #            define SIMDPP_PP_CONFIG_FLAGS() (SIMDPP_PP_CONFIG_MSVC())
 #        else
-#            error Dispatching is not supported on this compiler
+#            define SIMDPP_PP_CONFIG_FLAGS() (SIMDPP_PP_CONFIG_EDG() | SIMDPP_PP_CONFIG_STRICT())
 #        endif
+#    elif defined(_MSC_VER) && defined(__clang__)
+#        define SIMDPP_PP_CONFIG_FLAGS() (SIMDPP_PP_CONFIG_STRICT())
 #    elif defined(__MWERKS__)
 #        define SIMDPP_PP_CONFIG_FLAGS() (SIMDPP_PP_CONFIG_MWCC())
 #    elif defined(__DMC__)
-#        error Dispatching is not supported on this compiler
+#        define SIMDPP_PP_CONFIG_FLAGS() (SIMDPP_PP_CONFIG_DMC())
 #    elif defined(__BORLANDC__) && __BORLANDC__ >= 0x581
 #        define SIMDPP_PP_CONFIG_FLAGS() (SIMDPP_PP_CONFIG_STRICT())
 #    elif defined(__BORLANDC__) || defined(__IBMC__) || defined(__IBMCPP__) || defined(__SUNPRO_CC)
 #        define SIMDPP_PP_CONFIG_FLAGS() (SIMDPP_PP_CONFIG_BCC())
-#    elif defined(_MSC_VER) && !defined(__clang__)
+#    elif defined(_MSC_VER)
 #        define SIMDPP_PP_CONFIG_FLAGS() (SIMDPP_PP_CONFIG_MSVC())
 #    else
 #        define SIMDPP_PP_CONFIG_FLAGS() (SIMDPP_PP_CONFIG_STRICT())
@@ -73,12 +71,40 @@
 # /* SIMDPP_PP_VARIADICS */
 #
 # define SIMDPP_PP_VARIADICS_MSVC 0
-# define SIMDPP_PP_VARIADICS 1
-#
-#    if defined _MSC_VER && _MSC_VER >= 1400 && (!defined __EDG__ || defined(__INTELLISENSE__)) && !defined __clang__
+# if !defined SIMDPP_PP_VARIADICS
+#    /* variadic support explicitly disabled for all untested compilers */
+#    if defined __GCCXML__ || defined __CUDACC__ || defined __PATHSCALE__ || defined __DMC__ || defined __CODEGEARC__ || defined __BORLANDC__ || defined __MWERKS__ || ( defined __SUNPRO_CC && __SUNPRO_CC < 0x5120 ) || defined __HP_aCC && !defined __EDG__ || defined __MRC__ || defined __SC__ || defined __IBMCPP__ || defined __PGI
+#        define SIMDPP_PP_VARIADICS 0
+#    elif defined(_MSC_VER) && defined(__clang__)
+#        define SIMDPP_PP_VARIADICS 1
+#    /* VC++ (C/C++) and Intel C++ Compiler >= 17.0 with MSVC */
+#    elif defined _MSC_VER && _MSC_VER >= 1400 && (!defined __EDG__ || defined(__INTELLISENSE__) || defined(__INTEL_COMPILER) && __INTEL_COMPILER >= 1700)
 #        define SIMDPP_PP_VARIADICS 1
 #        undef SIMDPP_PP_VARIADICS_MSVC
 #        define SIMDPP_PP_VARIADICS_MSVC 1
+#    /* Wave (C/C++), GCC (C++) */
+#    elif defined __WAVE__ && __WAVE_HAS_VARIADICS__ || defined __GNUC__ && defined __GXX_EXPERIMENTAL_CXX0X__ && __GXX_EXPERIMENTAL_CXX0X__
+#        define SIMDPP_PP_VARIADICS 1
+#    /* EDG-based (C/C++), GCC (C), and unknown (C/C++) */
+#    elif !defined __cplusplus && __STDC_VERSION__ >= 199901L || __cplusplus >= 201103L
+#        define SIMDPP_PP_VARIADICS 1
+#    else
+#        define SIMDPP_PP_VARIADICS 0
 #    endif
+# elif !SIMDPP_PP_VARIADICS + 1 < 2
+#    undef SIMDPP_PP_VARIADICS
+#    define SIMDPP_PP_VARIADICS 1
+#    if defined _MSC_VER && _MSC_VER >= 1400 && !defined(__clang__) && (defined(__INTELLISENSE__) || (defined(__INTEL_COMPILER) && __INTEL_COMPILER >= 1700) || !(defined __EDG__ || defined __GCCXML__ || defined __CUDACC__ || defined __PATHSCALE__ || defined __DMC__ || defined __CODEGEARC__ || defined __BORLANDC__ || defined __MWERKS__ || defined __SUNPRO_CC || defined __HP_aCC || defined __MRC__ || defined __SC__ || defined __IBMCPP__ || defined __PGI))
+#        undef SIMDPP_PP_VARIADICS_MSVC
+#        define SIMDPP_PP_VARIADICS_MSVC 1
+#    endif
+# else
+#    undef SIMDPP_PP_VARIADICS
+#    define SIMDPP_PP_VARIADICS 0
+# endif
+#
+# if SIMDPP_PP_VARIADICS == 0
+#   error "This compiler has not been recognized as a supported compiler (lack of variadic support)"
+# endif
 #
 # endif
