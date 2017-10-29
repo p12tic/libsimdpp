ff024cdf675a56296b3f2a2eb124783fdde1a3ff
Povilas Kanapickas <povilas@radix.lt>
2017-10-30 00:24:26 +0200

    test: Move dispatch tests to separater directory

--- test/main_dispatcher.cc
+++ test/main_dispatcher.cc
@@ -5,7 +5,7 @@
             http://www.boost.org/LICENSE_1_0.txt)
 */
 
-#include "insn/dispatcher.h"
+#include "dispatcher/dispatcher.h"
 #include <algorithm>
 #include <iostream>
 #include <string>
