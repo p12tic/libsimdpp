93bdafde5a2f2801acfb1fecfcbf9492f7bae9c9
Povilas Kanapickas <povilas@radix.lt>
2017-12-02 11:38:26 +0200

    test: Improve store tests to properly check for out of bounds stores

--- test/utils/test_helpers.h
+++ test/utils/test_helpers.h
@@ -242,6 +242,15 @@ void test_push_internal(TestResultsSet& t, const simdpp::float64<N>& data,
 }
 
 template<class V>
+void test_push_stored(TestResultsSet& t, const typename V::element_type* data,
+                      unsigned count, const char* file, unsigned line)
+{
+    ElementType type = GetElementType<V>::value;
+    TestResultsSet::Result& res = t.push(type, count, file, line);
+    std::memcpy(res.data.data(), data, count * sizeof(data[0]));
+}
+
+template<class V>
 void print_vector_hex(std::ostream& out, const V& v)
 {
     simdpp::detail::mem_block<V> block(v);
@@ -270,6 +279,11 @@ void print_vector_numeric(std::ostream& out, const V& v)
 */
 #define TEST_PUSH(TC,T,D)                                                       \
     { test_push_internal((TC), (T)(D), __FILE__, __LINE__); }
+
+// The following macro is the same as TEST_PUSH, except that D is a pointer to
+// data of COUNT vectors of type T stored to memory. D must be V::element_type.
+#define TEST_PUSH_STORED(TC, T, D, COUNT)                                       \
+{ test_push_stored<V>((TC), (D), COUNT, __FILE__, __LINE__); }
 
 #define TEST_PUSH_ARRAY(TC, T, A)                                       \
 {                                                                       \
@@ -584,6 +598,28 @@ struct TemplateTestArrayHelper {
     }
 };
 
+template<class E>
+void test_cmp_memory(TestReporter& tr, const E* e1, const E* e2, unsigned count,
+                     bool expected_equal, unsigned line, const char* file)
+{
+    if (count == 0) {
+        tr.add_result(true);
+        return;
+    }
+
+    int memcmp_result = std::memcmp(e1, e2, sizeof(E) * count);
+    bool success = expected_equal ? memcmp_result == 0 : memcmp_result != 0;
+    tr.add_result(success);
+
+    if (!success) {
+        print_separator(tr.out());
+        print_file_info(tr.out(), file, line);
+        tr.out() << (expected_equal ? "Memory not equal:\n"
+                                    : "Memory equal:\n");
+        print_data_diff(tr.out(), GetElementType<E>::value, count, e1, e2);
+    }
+}
+
 template<class V1, class V2>
 void test_cmp_equal_impl(std::true_type /*is_V1_vector*/, TestReporter& tr,
                          const V1& q1, const V2& q2,
@@ -638,5 +674,11 @@ void test_cmp_equal(TestReporter& tr, const T1& a1, const T2& a2,
 
 #define TEST_NOT_EQUAL(TR, V1, V2)                                              \
     do { test_cmp_equal(TR, V1, V2, false, __LINE__, __FILE__); } while(0)
+
+#define TEST_EQUAL_MEMORY(TR, E1, E2, COUNT)                                    \
+    do { test_cmp_memory((TR), (E1), (E2), (COUNT), true, __LINE__, __FILE__); } while(0)
+
+#define TEST_NOT_EQUAL_MEMORY(TR, E1, E2, COUNT)                                \
+    do { test_cmp_memory((TR), (E1), (E2), (COUNT), false, __LINE__, __FILE__); } while(0)
 
 #endif
