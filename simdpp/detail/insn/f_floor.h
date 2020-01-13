1de4b647ea32a47a6f66b1b2406e29e80582be44
PearCoding <pearcoding@gmail.com>
2020-01-13 21:05:34 +0100

    Added bug fix of AVX512 for MSVC, clang and GCC for i_floor and i_ceil

--- simdpp/detail/insn/f_floor.h
+++ simdpp/detail/insn/f_floor.h
@@ -77,7 +77,11 @@ float32x8 i_floor(const float32x8& a)
 static SIMDPP_INL
 float32<16> i_floor(const float32<16>& a)
 {
-    return _mm512_floor_ps(a.native());
+#if SIMDPP_USE_SVML
+	return _mm512_floor_ps(a.native());
+#else
+	return _mm512_roundscale_ps(a.native(), (_MM_FROUND_TO_NEG_INF | _MM_FROUND_NO_EXC));
+#endif
 }
 #endif
 
@@ -148,7 +152,11 @@ float64x4 i_floor(const float64x4& a)
 static SIMDPP_INL
 float64<8> i_floor(const float64<8>& a)
 {
-    return _mm512_floor_pd(a.native());
+#if SIMDPP_USE_SVML
+	return _mm512_floor_pd(a.native());
+#else
+	return _mm512_roundscale_pd(a.native(), (_MM_FROUND_TO_NEG_INF | _MM_FROUND_NO_EXC));
+#endif
 }
 #endif
 
