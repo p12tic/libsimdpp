6103d6a1b249a6ae09aa476ee5400639f1b5a89c
Povilas Kanapickas <povilas@radix.lt>
2024-06-11 20:07:32 +0300

    Fix compile error in permute4() with integer arguments

--- simdpp/core/permute4.h
+++ simdpp/core/permute4.h
@@ -56,7 +56,7 @@ typename detail::get_expr_nomask<V>::type
         permute4(const any_vec16<N,V>& a)
 {
     static_assert(s0 < 4 && s1 < 4 && s2 < 4 && s3 < 4, "Selector out of range");
-    typename detail::get_expr_nomask<V>::type ra;
+    typename detail::get_expr_nomask_nosign<V>::type ra;
     ra = a.wrapped();
     return detail::insn::i_permute4<s0,s1,s2,s3>(ra);
 }
@@ -101,7 +101,7 @@ typename detail::get_expr_nomask<V>::type
         permute4(const any_vec32<N,V>& a)
 {
     static_assert(s0 < 4 && s1 < 4 && s2 < 4 && s3 < 4, "Selector out of range");
-    typename detail::get_expr_nomask<V>::type ra;
+    typename detail::get_expr_nomask_nosign<V>::type ra;
     ra = a.wrapped();
     return detail::insn::i_permute4<s0,s1,s2,s3>(ra);
 }
@@ -130,7 +130,7 @@ typename detail::get_expr_nomask<V>::type
         permute4(const any_vec64<N,V>& a)
 {
     static_assert(s0 < 4 && s1 < 4 && s2 < 4 && s3 < 4, "Selector out of range");
-    typename detail::get_expr_nomask<V>::type ra;
+    typename detail::get_expr_nomask_nosign<V>::type ra;
     ra = a.wrapped();
     return detail::insn::i_permute4<s0,s1,s2,s3>(ra);
 }
